03298ee90b5b4ce192191c82cd537ebe5326615a
陈德辉 <cdh@CDHdeiMac.local>
2018-01-29 16:18:19 +0800

    Initial Commit

---
+++ GitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTestDemo
+//
+//  Created by CDH on 2018/1/29.
+//  Copyright © 2018年 CDH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
